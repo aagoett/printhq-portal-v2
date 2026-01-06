b822e0248a480ea63da267eb44a149124ad447c3
aagoett <64336907+aagoett@users.noreply.github.com>
2026-01-06 11:53:39 -0800

    Update page.tsx

--- app/jobs/[id]/page.tsx
+++ app/jobs/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { createClient } from '@/utils/supabase/server';
+import { createClient } from '../../../utils/supabase/server';
 import JobInteractiveView from './JobInteractiveView';
 import { redirect, notFound } from 'next/navigation';
 
